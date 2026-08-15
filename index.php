23b76981b03d6096ed16dcf858f15ec5db138fd6
trtsmdr <trtsmdr.official@gmail.com>
2026-08-15 19:24:24 +0700

    test

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>BKI - Landing Page</title>
+    <title>BKI - Tirta Page</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
